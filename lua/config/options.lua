8c75497a07680bd17b2d8529188846522d61dcda
Fengtao Zhang <zhangfengtao1998@gmail.com>
2025-08-04 16:27:15 +0100

    macos version.

---
+++ lua/config/options.lua
@@ -0,0 +1,21 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+vim.opt.winbar = "%=%m %F"
+-- Set minimal number of screen lines above and below the cursor
+vim.opt.scrolloff = 10 -- Keeps at least 10 lines above/below cursor
+vim.opt.sidescrolloff = 25 --Keep at least 25 col on the left.
+
+-- Tab settings: expand tab to spaces, and set width to 4
+vim.opt.expandtab = true -- Convert tabs to spaces
+vim.opt.tabstop = 4 -- Number of spaces a tab counts for
+vim.opt.shiftwidth = 4 -- Number of spaces to use for autoindent
+vim.opt.softtabstop = 4 -- Number of spaces a tab counts for while editing
+
+-- Set cursor to blink during nvim.
+vim.opt.guicursor = {
+  "n-v-c:block-blinkon500-blinkoff500",
+  "i-ci:ver25-blinkon500-blinkoff500",
+  "r-cr:hor20-blinkon500-blinkoff500",
+  "o:hor40-blinkon500-blinkoff500",
+}
---
+++ lua/plugins/smear_cursor.lua
@@ -0,0 +1,16 @@
+return {
+  "sphamba/smear-cursor.nvim",
+
+  opts = {
+    smear_between_buffers = true,
+    smear_between_neighbor_lines = true,
+    scroll_buffer_space = true,
+    legacy_computing_symbols_support = false,
+    smear_insert_mode = true,
+    stiffness = 0.8,
+    trialing_stiffness = 0.5,
+    stiffness_insert_mode = 0.6,
+    trialing_stiffness_insert_mode = 0.6,
+    distance_stop_animating = 0.5,
+  },
+}

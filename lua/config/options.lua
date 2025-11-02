2d56b11c3bb2a2e2c09d251ec7ea5135a0273af6
Fengtao Zhang <zhangfengtao1998@gmail.com>
2025-11-02 12:43:13 +0000

    Linux updated

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,13 +4,13 @@
 vim.opt.winbar = "%=%m %F"
 -- Set minimal number of screen lines above and below the cursor
 vim.opt.scrolloff = 10 -- Keeps at least 10 lines above/below cursor
-vim.opt.sidescrolloff = 200 --Keep at least 5 col on the left.
+vim.opt.sidescrolloff = 900 --Keep at least 5 col on the left.
 
 -- Tab settings: expand tab to spaces, and set width to 4
 vim.opt.expandtab = true -- Convert tabs to spaces
-vim.opt.tabstop = 4 -- Number of spaces a tab counts for
-vim.opt.shiftwidth = 4 -- Number of spaces to use for autoindent
-vim.opt.softtabstop = 4 -- Number of spaces a tab counts for while editing
+vim.opt.tabstop = 2 -- Number of spaces a tab counts for
+vim.opt.shiftwidth = 2 -- Number of spaces to use for autoindent
+vim.opt.softtabstop = 2 -- Number of spaces a tab counts for while editing
 
 -- Set cursor to blink during nvim.
 vim.opt.guicursor = {

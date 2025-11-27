c42ac430856d33b72c598e1f67d582db7b8d57b0
H_win_fengtao <zhangfengtao1998@gmail.com>
2025-11-27 22:04:25 +0000

    Nice one Changes to be committed: 	modified:   .gitignore 	deleted:    lazy-lock.json 	deleted:    lazyvim.json 	modified:   lua/plugins/color_theme.lua

--- lua/plugins/color_theme.lua
+++ lua/plugins/color_theme.lua
@@ -1,16 +1,3 @@
-return {
-  {
-    "catppuccin/nvim",
-  },
-  lazy = false,
-  name = "catppuccin",
-  {
-    "lazyVim/LazyVim",
-    opts = {
-      colorscheme = "catppuccin-mocha",
-    },
-  },
-}
 -- return {
 --   {
 --     "catppuccin/nvim",
@@ -21,6 +8,26 @@ return {
 --     "lazyVim/LazyVim",
 --     opts = {
 --       colorscheme = "catppuccin-mocha",
---     },
+-- },
 --   },
 -- }
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+  },
+  {
+    "ellisonleao/gruvbox.nvim",
+    priority = 100,
+    config = true,
+  },
+  lazy = false,
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      -- colorscheme = "gruvbox",
+      colorscheme = "catppuccin-mocha",
+    },
+  },
+}
